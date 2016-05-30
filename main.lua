1589ff2c0fefaf5931c4fd9d6d6a25a3d95fbb98
LexLoki <pietroribeiropepe@hotmail.com>
2016-05-30 04:01:50 -0300

    MoreMain

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+
+
+local function printTable(t,offset)
+	offset = offset or ''
+	for i,v in pairs(t) do
+		if(type(v)=='table') then
+			print(offset ..i ..' :')
+			printTable(v,offset..'\t')
+		else
+			print(offset ..i ..' : '..tostring(v))
+		end
+	end
+end
+
 local tableIO = require('tableIO')
 
 local tableToBeSaved = {
@@ -14,3 +28,6 @@ for i=1,10 do
 end
 
 tableIO.save(tableToBeSaved,'savedTable.lua')
+
+local tableRead = require 'savedTable'
+printTable(tableRead)
